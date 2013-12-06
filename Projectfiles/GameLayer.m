a2d156c7fb27c31d69134e408477bb1871cc2427
Daniel Liu <hpec@qq.com>
2013-12-06 11:45:02 -0800

    init

---
+++ Projectfiles/GameLayer.m
@@ -0,0 +1,251 @@
+/*
+ * Kobold2D™ --- http://www.kobold2d.org
+ *
+ * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
+ */
+
+#import "GameLayer.h"
+#import "SimpleAudioEngine.h"
+#define WIDTH_WINDOW 480
+#define HEIGHT_WINDOW 320
+#define MARGIN 20
+#define NUM_ROWS 4
+#define NUM_COLUMNS 6
+#define DELAY_IN_SECONDS 0.15
+
+@interface GameLayer (PrivateMethods)
+@end
+
+NSMutableArray* grid;
+NSMutableArray* mice;
+int score;
+bool died;
+
+
+@implementation GameLayer
+
+@synthesize helloWorldString, helloWorldFontName;
+@synthesize helloWorldFontSize;
+
+-(id) init
+{
+	if ((self = [super init]))
+	{
+        CCLOG(@"%@ init", NSStringFromClass([self class]));
+        
+        CCSprite *sprite = [CCSprite spriteWithFile:@"grass_texture.jpg"];
+        sprite.anchorPoint = CGPointZero;
+        [self addChild:sprite z:-1];
+
+        
+        died = false;
+        grid = [[NSMutableArray alloc] init];
+        for (int row = 0; row < NUM_ROWS; row++ )
+        {
+            NSMutableArray* subArr = [[NSMutableArray alloc] init ];
+            for (int col = 0; col < NUM_COLUMNS; col++ )
+            {
+                NSNumber *item = @0;
+                [subArr addObject:item];
+            }
+            [grid addObject:subArr];
+        }
+        
+				
+		CCDirector* director = [CCDirector sharedDirector];
+
+		// get the hello world string from the config.lua file
+		[KKConfig injectPropertiesFromKeyPath:@"HelloWorldSettings" target:self];
+		
+		CCLabelTTF* label = [CCLabelTTF labelWithString:helloWorldString 
+											   fontName:helloWorldFontName 
+											   fontSize:helloWorldFontSize];
+		label.position = director.screenCenter;
+		label.color = ccGREEN;
+		[self addChild:label];
+
+		// print out which platform we're on
+		NSString* platform = @"(unknown platform)";
+		
+		if (director.currentPlatformIsIOS)
+		{
+			// add code 
+			platform = @"iPhone/iPod Touch";
+			
+			if (director.currentDeviceIsIPad)
+				platform = @"iPad";
+
+			if (director.currentDeviceIsSimulator)
+				platform = [NSString stringWithFormat:@"%@ Simulator", platform];
+		}
+		else if (director.currentPlatformIsMac)
+		{
+			platform = @"Mac OS X";
+		}
+		
+		CCLabelTTF* platformLabel = nil;
+		if (director.currentPlatformIsIOS) 
+		{
+			// how to add custom ttf fonts to your app is described here:
+			// http://tetontech.wordpress.com/2010/09/03/using-custom-fonts-in-your-ios-application/
+			float fontSize = (director.currentDeviceIsIPad) ? 48 : 28;
+			platformLabel = [CCLabelTTF labelWithString:platform 
+											   fontName:@"Ubuntu Condensed"
+											   fontSize:fontSize];
+		}
+		else if (director.currentPlatformIsMac)
+		{
+			// Mac builds have to rely on fonts installed on the system.
+			platformLabel = [CCLabelTTF labelWithString:platform 
+											   fontName:@"Zapfino" 
+											   fontSize:32];
+		}
+
+		platformLabel.position = director.screenCenter;
+		platformLabel.color = ccYELLOW;
+		[self addChild:platformLabel];
+		
+		id movePlatform = [CCMoveBy actionWithDuration:0.2f 
+											  position:CGPointMake(0, 50)];
+		[platformLabel runAction:movePlatform];
+        
+        
+		glClearColor(0.2f, 0.2f, 0.4f, 1.0f);
+        
+        [self schedule:@selector(nextFrame) interval:DELAY_IN_SECONDS];
+        
+	}
+
+	return self;
+}
+
+-(void) draw
+{
+//    float cellWidth = (WIDTH_WINDOW - 2 * MARGIN) / NUM_COLUMNS;
+//    float cellHeight = (HEIGHT_WINDOW - 2 * MARGIN) / NUM_ROWS;
+//    ccColor4F rectColor = ccc4f(0.5, 0.5, 0.5, 1.0);
+//    for (int i = 0; i < NUM_COLUMNS; i++) {
+//        for (int j = 0; j < NUM_ROWS; j++) {
+//            ccDrawRect(ccp(MARGIN + i * cellWidth, MARGIN + j * cellHeight), ccp(MARGIN + (i+1) * cellWidth, MARGIN + (j+1) * cellHeight));
+//        }
+//    }
+//    glEnable(GL_LINE_SMOOTH);
+//    glColor4ub(255, 255, 255, 255);
+//    glLineWidth(2);
+//    CGPoint vertices2[] = { ccp(79,299), ccp(134,299), ccp(134,229), ccp(79,229) };
+//    ccDrawPoly(vertices2, 4, YES);
+
+}
+
+
+-(void) update: (ccTime) delta
+{
+    
+    KKInput* input = [KKInput sharedInput];
+    CGPoint pos = [input locationOfAnyTouchInPhase:KKTouchPhaseAny];
+    
+//    if ([input anyTouchBeganThisFrame])
+//    {
+//        int x = pos.x;
+//        int y = pos.y;
+//        if(x >= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+//            if (done == true)
+//            {
+//                done = false;
+//            }
+//            else
+//            {
+//                done = true;
+//            }
+//            else if (x <= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET) {
+//                [self resetArrays];
+//                done = true;
+//            }
+//            else
+//            {
+//                int row = (y - Y_OFF_SET) / CELL_WIDTH;
+//                int col = x / CELL_WIDTH;
+//                
+//                if([[[grid objectAtIndex:row] objectAtIndex: col] integerValue] == 1)
+//                {
+//                    [[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@0];
+//                }
+//                else
+//                {
+//                    [[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@1];
+//                }
+//            }
+//    }
+//    
+//    
+//    else if ([input anyTouchEndedThisFrame])
+//    {
+//        priorX = 500;
+//        priorY = 500;
+//    }
+    
+//    if([input touchesAvailable])
+//    {
+//		{
+//			float x = pos.x;
+//			float y = pos.y;
+//			
+//			if(x >= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+//			{
+//			}
+//			else if(x <= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+//			{
+//			}
+//			else
+//			{
+//				int row = (int)(y - Y_OFF_SET) / CELL_WIDTH;
+//				int col = (int)x / CELL_WIDTH;
+//				float y1 = (float) ((y - 21.0) / 20.0);
+//				float x1 = (float) (x / 20.0);
+//				float cellYSmall = row;
+//				float cellYBig = row + 1;
+//				float cellXSmall = col;
+//				float cellXBig = col + 1;
+//				
+//				if ((priorX != 500.0)
+//					&& ((priorX < cellXSmall && x1 > cellXSmall && (double) row <= y1 && (double) row >= y1 - 1.0)
+//                        || (priorX > cellXBig && x1 < cellXBig && (double) row <= y1 && (double) row >= y1 - 1.0)
+//                        || (priorY > cellYBig && y1 < cellYBig && (double) col <= x1 && (double) col >= x1 - 1.0)
+//                        || (priorY < cellYSmall && y1 > cellXSmall && (double) col <= x1 && (double) col >= x1 - 1.0))
+//					)
+//				{
+//                    
+//					//make corresponding position in array alive or dead
+//					if([[[grid objectAtIndex:row] objectAtIndex: col] integerValue] == 1)
+//					{
+//						[[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@0];
+//                        //an abbreviation for [NSNumber numberWithInt: 0] is @0
+//					}
+//					else
+//					{
+//						[[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@1];
+//                        //an abbreviation for [NSNumber numberWithInt: 1] is @1
+//					}
+//				}
+//				priorX = x1;
+//				priorY = y1;
+//			}
+//		}
+//    }
+    
+}
+
+-(void) nextFrame
+{
+    if (!died) {
+        [self addMouse];
+    }
+}
+
+-(void) addMouse
+{
+    
+}
+
+@end
