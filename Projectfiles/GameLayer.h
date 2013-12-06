59244276a010614ba9f3a6c7d903d56b77666da1
Your Name <you@example.com>
2013-12-06 12:42:40 -0800

    lol

--- Projectfiles/GameLayer.h
+++ Projectfiles/GameLayer.h
@@ -14,6 +14,7 @@
 	int helloWorldFontSize;
 }
 
+
 @property (nonatomic, copy) NSString* helloWorldString;
 @property (nonatomic, copy) NSString* helloWorldFontName;
 @property (nonatomic) int helloWorldFontSize;
